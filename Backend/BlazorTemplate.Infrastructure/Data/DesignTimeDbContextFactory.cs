7878ebe68e5c55a10c0f0d613d1cdc18d3b8c6e0
Niroj <snero90@gmail.com>
2025-10-06 20:23:13 +0530

    added design time dbcontext factory and initial migration

---
+++ Backend/BlazorTemplate.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace BlazorTemplate.Infrastructure.Data
+{
+    internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            // Get API project directory
+            var basePath = Path.Combine(Directory.GetCurrentDirectory(), "../BlazorTemplate.Api");
+
+            // Load configuration from the API project's appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Get connection string
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
