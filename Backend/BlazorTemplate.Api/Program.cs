40da792ab68b27e4fd794c338aaf0394d3f13e46
Niroj <snero90@gmail.com>
2025-10-11 20:07:55 +0530

    moved wether forcast to separate endpoint

--- Backend/BlazorTemplate.Api/Program.cs
+++ Backend/BlazorTemplate.Api/Program.cs
@@ -2,6 +2,7 @@ using BlazorTemplate.Api.Middleware;
 using BlazorTemplate.Application;
 using BlazorTemplate.Infrastructure;
 using Scalar.AspNetCore;
+using BlazorTemplate.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = new ConfigurationBuilder()
@@ -28,28 +29,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Map endpoints
+app.MapWeatherForecast();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
