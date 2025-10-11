2b7b68b8d912ac7802557bcc4c073a64d78bd252
Niroj <snero90@gmail.com>
2025-10-11 19:34:24 +0530

    mediator added

--- Backend/BlazorTemplate.Application/DependencyInjection.cs
+++ Backend/BlazorTemplate.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using BlazorTemplate.Domain;
 using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
 
 namespace BlazorTemplate.Application;
 
@@ -8,7 +10,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddDomainServices();
+        // Register MediatR handlers from this assembly
+        services.AddMediatR(Assembly.GetExecutingAssembly());
         return services;
     }
-
 }
