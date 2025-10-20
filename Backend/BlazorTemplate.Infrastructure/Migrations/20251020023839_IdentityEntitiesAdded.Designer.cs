582a3dc9ac5a4d7ed98899292d67f2bf9ea60442
Niroj <snero90@gmail.com>
2025-10-20 08:09:10 +0530

    identity migration added

--- Backend/BlazorTemplate.Infrastructure/Migrations/20251020023839_IdentityEntitiesAdded.Designer.cs
+++ Backend/BlazorTemplate.Infrastructure/Migrations/20251020023839_IdentityEntitiesAdded.Designer.cs
@@ -4,6 +4,7 @@ using BlazorTemplate.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorTemplate.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251020023839_IdentityEntitiesAdded")]
+    partial class IdentityEntitiesAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
