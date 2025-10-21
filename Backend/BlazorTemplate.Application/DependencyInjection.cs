b88b1dc2f840a6c1dad035aa9a59d511ed2f4c0b
Niroj <snero90@gmail.com>
2025-10-21 09:57:02 +0530

    fixed mediatr registry

--- Backend/BlazorTemplate.Application/DependencyInjection.cs
+++ Backend/BlazorTemplate.Application/DependencyInjection.cs
@@ -9,8 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddDomainServices();
-        // Register MediatR handlers from this assembly using marker type
-        services.AddMediatR(typeof(DependencyInjection));
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
         return services;
     }
 }
