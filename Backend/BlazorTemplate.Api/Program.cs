753c6dff52008ff94d666743b137a7b8bb97b5c7
Niroj <snero90@gmail.com>
2025-11-14 11:31:34 +0530

    nswag installed

--- Backend/BlazorTemplate.Api/Program.cs
+++ Backend/BlazorTemplate.Api/Program.cs
@@ -1,8 +1,6 @@
 using BlazorTemplate.Api.Middleware;
 using BlazorTemplate.Application;
 using BlazorTemplate.Infrastructure;
-using Scalar.AspNetCore;
-using BlazorTemplate.Api.Endpoints;
 using BlazorTemplate.Api.Infrastructure;
 using BlazorTemplate.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
@@ -20,16 +18,19 @@ builder.Services.AddAuthorization();
 builder.Services.AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi(); // UseSwaggerUI Protected by if (env.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
